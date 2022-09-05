ad895034688fa3d871a7d8a88bb88c37d01ab672
GOD OF THUNDER <94171895+hkcs812@users.noreply.github.com>
2022-09-05 20:22:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ class Config(object):
     API_HASH = os.environ.get("API_HASH")
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
     BOT_NAME = os.environ.get("BOT_NAME")
-    PICS = (environ.get('PICS', 'https://telegra.ph/file/ebe8da68b5d8bfb0f4548.jpg https://telegra.ph/file/a7fbbb10d2f79af0de2b5.jpg https://telegra.ph/file/59dd925676b800f087879.jpg')
+    PICS = (environ.get('PICS', 'https://telegra.ph/file/ebe8da68b5d8bfb0f4548.jpg https://telegra.ph/file/a7fbbb10d2f79af0de2b5.jpg https://telegra.ph/file/59dd925676b800f087879.jpg'))
     FORCE_CHANNEL = environ.get('FORCE_CHANNEL', '-1001696817546')
